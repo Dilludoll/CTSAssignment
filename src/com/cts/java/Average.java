58c82d1a63693ed412454b2b77717abf72f85e31
abbas886 <abbas886@yahoo.com>
2018-05-24 16:37:35 +0530

    Class name confusing...wrong condition.

--- src/com/cts/java/Average.java
+++ src/com/cts/java/Average.java
@@ -1,9 +1,12 @@
 package com.cts.java;
 
+//This is not calculating average
+//You are getting grade based on 3 subject marks
+//So class name should note be Average.
 public class Average {
 	public String sum(int x,int y,int z) {
 		int sum=x+y+z;
-		
+		//From where you got this coniditon?  In the assignment document it is not present.
 		if(sum>=95) {
 			return "A";
 		}
