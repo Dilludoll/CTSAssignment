cfc8f06fcb1600d0a0d23eef9898fef030db6582
abbas886 <abbas886@yahoo.com>
2018-05-24 16:49:23 +0530

    //try to reduce number of iteration

--- src/com/cts/java/Prime.java
+++ src/com/cts/java/Prime.java
@@ -3,6 +3,7 @@ package com.cts.java;
 public class Prime {
 	public  boolean isPrime(int n) {
 		//boolean isprime =true;
+		//try to reduce number of iteration
 		for(int i=2;i<n;i++) {
 		if(n%i==0) {
 		
