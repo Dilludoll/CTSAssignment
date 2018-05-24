8ae09f5d3e42805327a36ad990fb8b5899bf1187
abbas886 <abbas886@yahoo.com>
2018-05-24 16:39:21 +0530

    method name not meaning full...why multiple if conditions??
    method name not meaning full...why multiple if conditions??

--- src/com/cts/java/Even.java
+++ src/com/cts/java/Even.java
@@ -1,12 +1,15 @@
 package com.cts.java;
 
 public class Even {
+	
+	//the method name should be meaning.  like isEvent, which should return true or false.
 	public int num(int x) {
 		int odd=0;
 		if(x%2==0) {
 		return x;
 		
 	}
+		//why so many conditions??
 		
 		else if(x%2!=0) {	
 			return odd;
