1a6893c8d78f8a16bb95c8577728a5a43b038b74
abbas886 <abbas886@yahoo.com>
2018-05-24 16:50:35 +0530

    incomplete program

--- src/com/cts/java/Square.java
+++ src/com/cts/java/Square.java
@@ -1,6 +1,9 @@
 package com.cts.java;
 
 public class Square {
+	//incomplete program
+	//method should not getSquare
+	//You can use displaySquare
 	public static void getSquare(int x) {
 		for(i=)
 	}
